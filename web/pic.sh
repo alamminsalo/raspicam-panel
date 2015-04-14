e7d0a4f74722e17298e82b122d96dcaa39c1b6a3
dalibomba <antti.lamminsalo@gmail.com>
2015-04-14 09:11:49 +0300

    finishing

--- web/pic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd tmp
-
-ls | grep -v $2 | xargs rm
-
-raspistill -w 320 -h 240 -t 500 -o $1 &
-
--- web/stream.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd tmp
-
-mkfifo buf.webm
-
-raspivid -ex night -w 320 -h 240 -t 2000 -fps 25 -o - | ffmpeg -i pipe:0 -y -vcodec libvpx -s 320x240 buf.webm > /dev/null 2>&1 &
-
